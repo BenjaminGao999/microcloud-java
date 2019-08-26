be55110172554cbd94670956221d4d6e61ecdf36
benjaminGzq <gzqytu@163.com>
2019-08-26 17:04:23 +0800

    1. 服务发现eureka, 以及添加eureka-server security 2. eureka-server高可用， 部署3台eureka-server

---
+++ microcloud-eureka-7003/src/main/java/com/example/microcloud/microcloudeureka7003/MicrocloudEureka7003Application.java
@@ -0,0 +1,15 @@
+package com.example.microcloud.microcloudeureka7003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicrocloudEureka7003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocloudEureka7003Application.class, args);
+	}
+
+}
---
+++ microcloud-eureka-7003/src/test/java/com/example/microcloud/microcloudeureka7003/MicrocloudEureka7003ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microcloud.microcloudeureka7003;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrocloudEureka7003ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
